1710e69e0c0348b2074eeb39f7e53e34df43a518
Rafael Enes <rafaeleneswork@gmail.com>
2021-10-20 06:51:54 -0300

    Criando repository e service da categoria

--- src/main/java/com/rafaelenes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rafaelenes/cursomc/resources/CategoriaResource.java
@@ -1,30 +1,34 @@
 package com.rafaelenes.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafaelenes.cursomc.domain.Categoria;
+import com.rafaelenes.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria (1, "Informática");
-		Categoria cat2 = new Categoria (2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
+//	@GetMapping("/{id}") NÃO FUNCIONA ASSIM POIS NÃO DA PRA CONVERTER DE <CATEGORIA> PARA OPTIONAL
+//	public ResponseEntity<?> find(@PathVariable Integer id) {
+//		Optional<Categoria> obj = service.buscar(id);
+//		return ResponseEntity.ok().body(obj);
+//	}
 	
 }
---
+++ src/main/java/com/rafaelenes/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.rafaelenes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafaelenes.cursomc.domain.Categoria;
+import com.rafaelenes.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) { 
+		 Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElse(null); 
+		}
+
+}
