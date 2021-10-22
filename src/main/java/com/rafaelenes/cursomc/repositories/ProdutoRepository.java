b8f5aa16bc44d9b3fefc1860ec53bdbeb94db4a2
Rafael Enes <rafaeleneswork@gmail.com>
2021-10-22 08:06:27 -0300

    relacionando produto com categoria

--- src/main/java/com/rafaelenes/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/rafaelenes/cursomc/repositories/ProdutoRepository.java
@@ -3,9 +3,9 @@ package com.rafaelenes.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.rafaelenes.cursomc.domain.Categoria;
+import com.rafaelenes.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
-
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+		
 }
