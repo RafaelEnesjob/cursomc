0d5afcedda32d669ea00a9aaedf581174182417d
Rafael Enes <rafaeleneswork@gmail.com>
2022-06-25 17:53:16 -0300

    inserindo nova categoria

--- src/main/java/com/rafaelenes/cursomc/services/CategoriaService.java
+++ src/main/java/com/rafaelenes/cursomc/services/CategoriaService.java
@@ -20,4 +20,9 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 			"Id informado não existe " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+
+    public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return categoriaRepository.save(obj);
+    }
 }
