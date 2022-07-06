7067be91ae0be2918f06bbe1d7d61cefd27d5d82
Rafael Enes <rafaeleneswork@gmail.com>
2022-07-06 08:28:45 -0300

    personalizando o retorno da validação

---
+++ src/main/java/com/rafaelenes/cursomc/resources/exception/FieldsMessage.java
@@ -0,0 +1,34 @@
+package com.rafaelenes.cursomc.resources.exception;
+
+import java.io.Serializable;
+
+public class FieldsMessage implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String fieldName;
+    private String message;
+
+    public FieldsMessage() {
+    }
+
+    public FieldsMessage(String fieldName, String message) {
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
