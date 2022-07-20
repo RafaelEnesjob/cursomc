d73b63866e8da41aa024f7ca93784832795c2377
Rafael Enes <rafaeleneswork@gmail.com>
2022-07-20 08:10:08 -0300

    correcoes em categoria e pagamento

--- src/main/java/com/rafaelenes/cursomc/services/CategoriaService.java
+++ src/main/java/com/rafaelenes/cursomc/services/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.rafaelenes.cursomc.services;
 
 import com.rafaelenes.cursomc.domain.Categoria;
+import com.rafaelenes.cursomc.domain.Cliente;
 import com.rafaelenes.cursomc.dto.CategoriaDTO;
 import com.rafaelenes.cursomc.repositories.CategoriaRepository;
 import com.rafaelenes.cursomc.services.exceptions.DataIntegrityException;
@@ -33,8 +34,9 @@ public class CategoriaService {
     }
 
     public Categoria update(Categoria obj) {
-        findById(obj.getId());
-        return categoriaRepository.save(obj);
+        Categoria newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return categoriaRepository.save(newObj);
     }
 
     public void delete(Integer id) {
@@ -58,5 +60,9 @@ public class CategoriaService {
 
     public Categoria fromDTO(CategoriaDTO objDto) {
         return new Categoria(objDto.getId(), objDto.getNome());
+    }
+
+    private void updateData(Categoria newObj, Categoria obj) {
+        newObj.setNome(obj.getNome());
     }
 }
